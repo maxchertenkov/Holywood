db52050e51979d66e97d2a549fadc3ad65fb0d3a
Maksym Chertenkov <maksymc@techfinancials.com>
2018-06-01 11:46:52 +0300

    Products pages

---
+++ themes/holywood/page-templates/product-item-page.php
@@ -0,0 +1,220 @@
+<?php
+/* Template Name: Product Item Page */
+
+get_header();
+?>
+<div class="product_item_page">
+    <div class="top_blk">
+        <div class="wrapper">
+            <h1 class="board_name">Seven samurai</h1>
+        </div>
+    </div>
+    <div class="wrapper product_wrap">
+        <div class="product_content">
+            <div class="product_discription_blk">
+                <h3 class="product_name">Seven samurai</h3>
+                <p class="product_description">Задача организации, в особенности же укрепление и развитие структуры представляет собой интересный эксперимент проверки дальнейших направлений развития. Значимость этих проблем настолько очевидна, что дальнейшее развитие различных форм деятельности требуют от нас анализа соответствующий условий активизации. Идейные соображения высшего порядка, а также консультация с широким активом позволяет выполнять важные задания по разработке соответствующий условий активизации.</p>
+                <div class="product_specs"><strong class="spec_title">Назначение </strong>All mountain</div>
+                <div class="product_specs"><strong class="spec_title">Места катания </strong>Укатанные трассы, вне трасс, парк</div>
+                <div class="product_specs"><strong class="spec_title">Преодолеваемые препятствия </strong>Ровный укатанный снег, пухлый снег, трамплины</div>
+                <div class="product_specs"><strong class="spec_title">Тип изгиба </strong>HYBRID CAMBER</div>
+                <div class="product_specs"><strong class="spec_title">Жесткость </strong>6/10</div>
+                <div class="product_specs"><strong class="spec_title">Геометрия </strong>DIRECTIONAL TWIN, смещение на одну пару крепежей с добавлением экстра ряда</div>
+                <div class="product_specs"><strong class="spec_title">Предлагаемые технологии </strong>Экстра ряд крепежей (семь рядов)</div>
+                <div class="product_specs"><strong class="spec_title">Уровень райдера </strong>Новичок-продвинутый</div>
+            </div>
+            <div class="camber_type">
+                <h4 class="description_title">Тип кембера</h4>
+                <img src="/wp-content/themes/holywood/images/dist/camber_img_1.png">
+                <img src="/wp-content/themes/holywood/images/dist/camber_type_2.png">
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Ростовка</th>
+                        <th>Толщина сердечника</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>151</td>
+                        <td>7</td>
+                    </tr>
+                    <tr>
+                        <td>151</td>
+                        <td>7</td>
+                    </tr>
+                    <tr>
+                        <td>151</td>
+                        <td>7</td>
+                    </tr>
+                    <tr>
+                        <td>151</td>
+                        <td>7</td>
+                    </tr>
+                    <tr>
+                        <td>151</td>
+                        <td>7</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Длина</th>
+                        <th>Длина рабочей зоны (CM)</th>
+                        <th>Эффективная длина канта (СМ)</th>
+                        <th>Ширина талии (CM)</th>
+                        <th>Ширина носа и хвоста (CM)</th>
+                        <th>Длина носа и хвоста (CM)</th>
+                        <th>Радиус бокового выреза (М)</th>
+                        <th>Смещение крепежей (ММ)</th>
+                        <th>Диапазон расстановки креплений (CM)</th>
+                        <th>Диапазон роста райдера (СМ)</th>
+                        <th>Рекомендованный вес райдера (КГ)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>151</td>
+                        <td>114</td>
+                        <td>116</td>
+                        <td>24.8</td>
+                        <td>28.86/28.86</td>
+                        <td>20/20</td>
+                        <td>7.6</td>
+                        <td>20</td>
+                        <td>46-62</td>
+                        <td>159-178</td>
+                        <td>53-79</td>
+                    </tr>
+                    <tr>
+                        <td>151</td>
+                        <td>114</td>
+                        <td>116</td>
+                        <td>24.8</td>
+                        <td>28.86/28.86</td>
+                        <td>20/20</td>
+                        <td>7.6</td>
+                        <td>20</td>
+                        <td>46-62</td>
+                        <td>159-178</td>
+                        <td>53-79</td>
+                    </tr>
+                    <tr>
+                        <td>151</td>
+                        <td>114</td>
+                        <td>116</td>
+                        <td>24.8</td>
+                        <td>28.86/28.86</td>
+                        <td>20/20</td>
+                        <td>7.6</td>
+                        <td>20</td>
+                        <td>46-62</td>
+                        <td>159-178</td>
+                        <td>53-79</td>
+                    </tr>
+                    <tr>
+                        <td>151</td>
+                        <td>114</td>
+                        <td>116</td>
+                        <td>24.8</td>
+                        <td>28.86/28.86</td>
+                        <td>20/20</td>
+                        <td>7.6</td>
+                        <td>20</td>
+                        <td>46-62</td>
+                        <td>159-178</td>
+                        <td>53-79</td>
+                    </tr>
+                    <tr>
+                        <td>151</td>
+                        <td>114</td>
+                        <td>116</td>
+                        <td>24.8</td>
+                        <td>28.86/28.86</td>
+                        <td>20/20</td>
+                        <td>7.6</td>
+                        <td>20</td>
+                        <td>46-62</td>
+                        <td>159-178</td>
+                        <td>53-79</td>
+                    </tr>
+                    <tr>
+                        <td>151</td>
+                        <td>114</td>
+                        <td>116</td>
+                        <td>24.8</td>
+                        <td>28.86/28.86</td>
+                        <td>20/20</td>
+                        <td>7.6</td>
+                        <td>20</td>
+                        <td>46-62</td>
+                        <td>159-178</td>
+                        <td>53-79</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="product_image_wrap">
+            <div class="product_big_image">
+                <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+            </div>
+            <div class="product_miniature_wrap">
+                <div class="item">
+                    <div class="image_wrap">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <span class="size">151</span>
+                </div>
+                <div class="item">
+                    <div class="image_wrap">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <span class="size">154</span>
+                </div>
+                <div class="item">
+                    <div class="image_wrap">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <span class="size">157</span>
+                </div>
+                <div class="item">
+                    <div class="image_wrap">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <span class="size">160</span>
+                </div>
+                <div class="item">
+                    <div class="image_wrap">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <span class="size">163</span>
+                </div>
+                <div class="item">
+                    <div class="image_wrap">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <span class="size">159W</span>
+                </div>
+                <div class="item">
+                    <div class="image_wrap">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <span class="size">161W</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
---
+++ themes/holywood/page-templates/products-page.php
@@ -0,0 +1,78 @@
+<?php
+/* Template Name: Products Page */
+
+get_header();
+?>
+<div class="products_page">
+    <section class="top_blk">
+        <div class="wrapper">
+            <h1 class="page_title">Доски</h1>
+            <p>Задача организации, в особенности же укрепление и развитие структуры представляет собой интересный эксперимент проверки дальнейших направлений развития. Значимость этих проблем настолько очевидна, что дальнейшее развитие различных форм деятельности требуют от нас анализа соответствующий условий активизации. Идейные соображения высшего порядка, а также консультация с широким активом позволяет выполнять важные задания по разработке соответствующий условий активизации.</p>
+        </div>
+    </section>
+    <section class="product_list_blk">
+        <div class="wrapper">
+            <div class="product_list_wrap">
+                <a class="item">
+                    <div class="deck_img">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <p>Доска 1</p>
+                </a>
+                <a class="item">
+                    <div class="deck_img">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <p>Доска 2</p>
+                </a>
+                <a class="item">
+                    <div class="deck_img">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <p>Доска 3</p>
+                </a>
+                <a class="item">
+                    <div class="deck_img">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <p>Доска 4</p>
+                </a>
+                <a class="item">
+                    <div class="deck_img">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <p>Доска 1</p>
+                </a>
+                <a class="item">
+                    <div class="deck_img">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <p>Доска 2</p>
+                </a>
+                <a class="item">
+                    <div class="deck_img">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <p>Доска 3</p>
+                </a>
+                <a class="item">
+                    <div class="deck_img">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_front.png">
+                        <img src="/wp-content/themes/holywood/images/dist/deck_1_back.png">
+                    </div>
+                    <p>Доска 4</p>
+                </a>
+            </div>
+        </div>
+    </section>
+</div>
+
+<?php
+get_footer();
